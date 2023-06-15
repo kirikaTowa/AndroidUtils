9172b09f81e835aca2bbc8da202122d967ad0a95
Yewj <2685872110@qq.com>
2023-06-15 09:28:43 +0800

    feat：BaseActivity改写弃用的onBackpress，MainAc继承BaseActivity

--- app/src/main/java/com/kakusummer/sample/MainActivity.kt
+++ app/src/main/java/com/kakusummer/sample/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.kakusummer.sample
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.assistant.bases.BaseActivity
 import com.kakusummer.androidutils.R
+import com.kakusummer.androidutils.databinding.ActivityMainBinding
 import com.kakusummer.sample.Dialog.TipUserPoliceDialog
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val layoutId: Int
+        get() = R.layout.activity_main
 
+
+    override fun initView() {
+        super.initView()
         var tipUserDialog:TipUserPoliceDialog?=null
         tipUserDialog= TipUserPoliceDialog(this@MainActivity) {
             if (it) {
