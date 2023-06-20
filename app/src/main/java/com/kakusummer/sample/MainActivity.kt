c78f828b0e973fe897eeeb2642fefe647b19eb12
Yewj <2685872110@qq.com>
2023-06-20 16:42:49 +0800

    feat：lifecycle感知生命周期

--- app/src/main/java/com/kakusummer/sample/MainActivity.kt
+++ app/src/main/java/com/kakusummer/sample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.kakusummer.sample
 
 import android.app.ProgressDialog
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.assistant.bases.BaseActivity
 import com.dawn.kotlinbasedemo.vm.netWorkViewModel
 import com.kakusummer.androidutils.R
 import com.kakusummer.androidutils.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -28,8 +30,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             this@MainActivity,
             ViewModelProvider.NewInstanceFactory()
         )[netWorkViewModel::class.java]
-
-        viewModel.requestData()
+        //虽然viewModel里执行的是异步请求，外层使用lifecycleScope.launch感知生命周期
+        lifecycleScope.launch {
+            viewModel.requestData()
+        }
     }
 
     override fun initListener() {
