1bf731ed1e40d54c439a09893fa4587d68a95379
ywj <2685872110@qq.com>
2024-06-25 11:08:44 +0800

    feat：壁纸测试功能

--- app/src/main/java/com/kakusummer/sample/ViewStubActivity.kt
+++ app/src/main/java/com/kakusummer/sample/ViewStubActivity.kt
@@ -1,10 +1,11 @@
 package com.kakusummer.sample
 
-import android.os.Bundle
+import android.app.WallpaperManager
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.DataBindingUtil
 import com.assistant.bases.BaseActivity
+import com.assistant.utils.ConvertUtils
 import com.kakusummer.androidutils.R
 import com.kakusummer.androidutils.databinding.ActivityViewStubBinding
 import com.kakusummer.androidutils.databinding.ViewStubDemoBinding
@@ -14,19 +15,24 @@ class ViewStubActivity : BaseActivity<ActivityViewStubBinding>() {
         get() = R.layout.activity_view_stub
     override val TAG: String
         get() = "TAG_VIEW_STUB"
-    lateinit var vsDemoBinding:ViewStubDemoBinding
+    lateinit var vsDemoBinding: ViewStubDemoBinding
 
     override fun initDate() {
         super.initDate()
         inflateViewStub()
 
-        vsDemoBinding.tvTextOne.text="你好"
+        //设置壁纸功能，在Fragment中可能会报那个无参Fragment问题
+        val res=AppCompatResources.getDrawable(this, R.drawable.ic_pic_2_audit_3)
+        val wallpaperManager = WallpaperManager.getInstance(this)
+        wallpaperManager.setBitmap(ConvertUtils.drawable2Bitmap(res))
+
+        vsDemoBinding.tvTextOne.text = "你好"
     }
 
     private fun inflateViewStub() {
         if (!binding.vsDemo.isInflated) {
-            val vsInflate: View? =  binding.vsDemo.viewStub?.inflate()
-            vsDemoBinding= vsInflate?.let {
+            val vsInflate: View? = binding.vsDemo.viewStub?.inflate()
+            vsDemoBinding = vsInflate?.let {
                 DataBindingUtil.getBinding(it)
             }!!
         }
